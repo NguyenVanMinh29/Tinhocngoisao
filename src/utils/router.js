4c196e7751c2c1a0a4e8baeccc6fbc5b74498280
NguyenVanMinh29 <minhminh74088@gmail.com>
2024-06-06 17:12:13 +0700

    VMinh

---
+++ src/utils/router.js
@@ -0,0 +1,14 @@
+const USER_ROUTES = {
+    HOME: "",
+    PROFILE: "thong-tin-ca-nhan",
+    BUILDNETS: "/lap-dat-net",
+    INSTALLMENT: "/tra-gop",
+    PRICELIST: "/bang-gia",
+    BUILDPC: "/xay-dung-cau-hinh",
+    POLICY: "/chinh-sach-bao-hanh",
+    MINES: "/thiet-bi-mining",
+};
+
+export const ROUTERS = {
+    USER: USER_ROUTES,
+};
